9a5d9361118d816dc4001316d6bfa19fdeaf0d5a
Angel-AV <84346357+Angel-AV@users.noreply.github.com>
2021-06-13 14:51:12 +0400

    Add files via upload

--- homework4.js
+++ homework4.js
@@ -53,12 +53,45 @@ console.log(numberOfTwo(4589966455, 9))
 
 
 
+// 3. Given two numbers. Print powers of 2 between that numbers. (without using Math.pow)
+ 
+function myFunc(num,num1) {
+  let a = 0
+    for (i=0; i < b; i++ )  {
+    a = (num + 1)*2
+    a++
+  }
+ return a
+}
+
+let num 
+let num1
+let a = num + 1
+for (let i = 0; i < num1; i++) {
+
+}
+
+
+
+//4 Insert a number. Print “yes” if it contains 3 successive zeros, otherwise print “no”.
+let a = +prompt();
+function threeZeros(a) {
+    let count = 1;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === a[i + 1] && a[i] === '0') {
+            count++;
+        }
+    }
+    return count === 3 ? 'Yes' : 'No';
+}
+console.log(threeZeros(a.toString()));
 
 
 
 
 //5. Insert two positive integers n and m between 1 and 10.
 // Compute and print n m .
+
 n = +prompt()
 m = +prompt()
 if(n%2 ===0, n > 1, n <10 && m % 2 ===0 && m > 1 && m <10) {
